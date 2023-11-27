49a3e80c344913e48de2311abf034ec65be498c5
Bipashree Aryal <aryalbipashree@gmail.com>
2023-11-27 22:39:34 +0545

    Added Utilities

---
+++ core/utilities/logger.go
@@ -0,0 +1,32 @@
+package utilities
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"strconv"
+	"github.com/AryalKTM/uptime-core/models"
+	"github.com/fatih/color"
+)
+
+func PrintStatus(service *models.Service, protocol *models.Protocol, err error) (string, error) {
+	now := time.Now()
+	port := strconv.Itoa(protocol.Port)
+	message := ""
+	if port == "0" {
+		port = "No Port"
+	}
+	if err != nil {
+		red := color.New(color.FgRed).SprintFunc()
+		message = fmt.Sprintf("[%s] [%s] [%s] [%s - %s - %s] An error has occured: %s", red("Error"), now.Format(time.RFC3339), service.Name, protocol.Type, protocol.Server, port, err.Error())
+	} else {
+		green := color.New(color.FgHiGreen).SprintFunc()
+		message = fmt.Sprintf("[%s] [%s] [%s] [%s - %s - %s] Service Seems OK.", green("OK"), now.Format(time.RFC3339), service.Name, protocol.Type, protocol.Server, port)
+	}
+	log.Println(message)
+	return message, err
+}
+
+func CreateColorString(str string, clr color.Attribute) string {
+	return color.New(clr).SprintFunc()(str)
+}
